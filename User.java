ee10a763bc1b136b30dc0193a88ec4e8b6836807
adamkilker <kilk23@yahoo.com>
2016-09-17 17:59:59 -0500

    BeerDiary

---
+++ User.java
@@ -0,0 +1,26 @@
+package beerdiary;
+
+public class User {
+
+	private String userName;
+	private String fName;
+	private String lName;
+
+	public User(String userName, String fName, String lName) {
+		this.userName = userName;
+		this.fName = fName;
+		this.lName = lName;
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public String getFName() {
+		return this.fName;
+	}
+
+	public String getLName() {
+		return this.lName;
+	}
+}
